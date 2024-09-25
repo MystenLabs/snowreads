fccb123117d05a2895a65010ca5871f950c8dc5b
DionisisLougaris <dlougaris@gmail.com>
2024-09-25 11:09:56 +0300

    Add abstract paper page as well as important layout components

---
+++ app/src/layout/Header.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Header: React.FC = () => {
+  return (
+    <header className="w-full bg-[#E4F0EF] border-b border-gray-300">
+      <div className="flex items-center p-4">
+      <div className="logo">
+          <img src="../public/logo.png" alt="Walrus Arxiv Logo" className="h-6" />
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
