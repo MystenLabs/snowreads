acec546ca1582934f2d3a0237b1059c8f5c54762
nikos-terzo <139557324+nikos-terzo@users.noreply.github.com>
2024-10-04 14:29:10 +0300

    Fetch real metadata (#22)
    * Artificial Intelligence in landing page

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -3,14 +3,43 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Custom404 from './pages/Custom404';
 import LandingPageLayout from './layout/landingLayout/LandingPageLayout';
 import CategoryListLayout from './layout/categoryListLayout/CategoryListLayout';
+import { useEffect, useState } from 'react';
+import { ISubCategory } from './interfaces/IAllPapers';
 
 
 function App() {
+  const [artificialIntelligence, setArtificialIntelligence] = useState<ISubCategory | null>(null);  // State to store fetched data
+  const [loading, setLoading] = useState(true);  // Loading state
+  const [error, setError] = useState<null | string>(null);  // Error state
+
+  useEffect(() => {
+    // Fetch the public JSON resource
+    fetch("/papers.json")  // Replace with the actual public API URL
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw response;
+      })
+      .then((data) => {
+          console.log(data);
+        setArtificialIntelligence(data['Computing Research Repository']['Artificial Intelligence']);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.log(error);
+        setError(error);
+        setLoading(false);
+      });
+
+  }, []);
+
+
   return (
     <div className='bg-primary '>
       <Router>
           <Routes>
-            <Route path="/" element={<LandingPageLayout />} />
+            <Route path="/" element={<LandingPageLayout artificialIntelligence={artificialIntelligence}/>} />
             <Route path="/abs/:doi" element={<PaperLayout />} />
             <Route path="/category/:title" element={<CategoryListLayout />} />
             <Route path="/collection/:title" element={<CategoryListLayout collections={true}/>} />
--- app/src/layout/landingLayout/LandingPageLayout.tsx
+++ app/src/layout/landingLayout/LandingPageLayout.tsx
@@ -1,12 +1,15 @@
 import Footer from '../Footer';
 import LandingPage from '../../pages/LandingPage';
+import { ISubCategory } from '../../interfaces/IAllPapers';
 
-function LandingPageLayout() {
+function LandingPageLayout({artificialIntelligence}: {
+    artificialIntelligence: ISubCategory | null;
+}) {
 
   return (
     <div className="flex flex-col h-screen">
       <div className="flex-grow overflow-y-auto">
-        <LandingPage />
+        <LandingPage artificialIntelligence={artificialIntelligence}/>
       </div>
       <Footer />
     </div>
