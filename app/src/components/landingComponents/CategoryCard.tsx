4e8532891281e2bfb6f8eb26a1ae23d00604cdf3
DionisisLougaris <dlougaris@gmail.com>
2024-10-11 10:25:01 +0300

    Deploy on 10/10

--- app/src/components/landingComponents/CategoryCard.tsx
+++ app/src/components/landingComponents/CategoryCard.tsx
@@ -15,13 +15,39 @@ const CategoryCard: React.FC<ICategoryCardProps> = ({
       className={`block p-4 rounded-lg text-center min-h-[180px] bg-white relative transform transition-transform duration-200 ease-in-out hover:scale-105 cursor-pointer ${className} 
                   w-full sm:w-full md:w-[90%] lg:w-auto`}
     >
+      {/* Conditionally apply the background images for different categories */}
+      {category === "Computer Science" && (
+        <div
+          className="absolute bottom-[-6px] right-0 w-[110px] h-[110px] bg-no-repeat bg-contain"
+          style={{ backgroundImage: `url('/disc_bg.png')` }}
+        />
+      )}
+      {category === "Mathematics" && (
+        <div
+          className="absolute bottom-[10px] right-[-73px] w-[150px] h-[150px] bg-no-repeat bg-contain"
+          style={{ backgroundImage: `url('/globe_bg.png')` }}
+        />
+      )}
+      {category === "Statistics" && (
+        <div
+          className="absolute bottom-[5px] right-[-84px] w-[170px] h-[170px] bg-no-repeat bg-contain"
+          style={{ backgroundImage: `url('/folder_bg.png')` }}
+        />
+      )}
+      {category === "Quantitative Finance" && (
+        <div
+          className="absolute bottom-[-17px] right-0 w-[130px] h-[130px] bg-no-repeat bg-contain"
+          style={{ backgroundImage: `url('/laptop_bg.png')` }}
+        />
+      )}
+
       <div className="flex">
         <div className="w-10 h-10 rounded-lg bg-teal-100 flex items-center justify-center mr-4">
           <img src={icon} alt={`${category} icon`} className="w-4 h-4" />
         </div>
         <div className="text-left max-w-[80%]">
           <span className="text-sm text-gray-500 block">{category}</span>
-          <p className="text-lg font-semibold text-gray-800 py-1 ">{title}</p>
+          <p className="text-lg font-semibold text-gray-800 py-1">{title}</p>
         </div>
       </div>
 
