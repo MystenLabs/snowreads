86c0d9c1c64af8eefb82f015adaa3afd9ae60dcd
nikos-terzo <139557324+nikos-terzo@users.noreply.github.com>
2024-10-11 16:29:06 +0300

    Collections json of randomly picked papers (#31)
    * Randomly filled collections.json and created collection-size.ts
    
    * Add collection size and check that indeed every collection has 10 papers

---
+++ publish/collection-size.ts
@@ -0,0 +1,27 @@
+import fs from 'fs';
+
+const COLLECTIONS_JSON = '../app/public/collections.json';
+const ABS_DIR = '../app/public/abs';
+
+let collections = JSON.parse(fs.readFileSync(COLLECTIONS_JSON, 'utf8'));
+
+for (let collection in collections) {
+    if (typeof collections[collection] !== 'object') {
+        console.log(`Collection ${collection} is not an object`);
+        break;
+    }
+    collections[collection].size = 0;
+    let paperCount = 0;
+    for (const paper of collections[collection].papers) {
+        const paperId = paper.id;
+        const absJson = JSON.parse(fs.readFileSync(`${ABS_DIR}/${paperId}.json`, 'utf8'));
+        collections[collection].size += parseInt(absJson.pdfSize);
+        paperCount++;
+    }
+
+    if (paperCount != 10) {
+        console.log(`Collection ${collection} has ${paperCount} papers`);
+    }
+}
+
+fs.writeFileSync(COLLECTIONS_JSON, JSON.stringify(collections));
