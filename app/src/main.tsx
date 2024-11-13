ec11380560eb6730312fa10db1cd50794c9c3edb
Dio <dlougaris@gmail.com>
2024-11-13 12:05:33 +0200

    Integrate amplitude analytics

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -2,6 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import * as amplitude from "@amplitude/analytics-browser";
+
+amplitude.init("b9c388dd082e3727ee05f3d4b79e0edc", { autocapture: true });
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
